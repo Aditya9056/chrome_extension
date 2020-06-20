cf34b3911358922ebb87bf6cd428effce9b9d4e8
Aditya Jain <vvasujain@gmail.com>
2020-06-20 16:50:38 +0530

    Final Release

---
+++ src/contentScript.js
@@ -0,0 +1,11 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    let darkmode = prompt("want dark mode", "Yes Or No");
+    
+    if(darkmode === "Yes"){
+        document.body.style.backgroundColor = "rgb(41, 41, 41)";
+        document.body.style.color = "rgb(255, 255, 255)";
+        var dark = "on";
+    }
+        
+});
